ec3c493f4c47698c818f46e0bfe977dd0339f4a4
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2021-09-29 13:04:29 +0200

    Nikola, Joacim, Christoffer

--- src/test/java/com/example/rockPaperScissorsTDD/GameTest.java
+++ src/test/java/com/example/rockPaperScissorsTDD/GameTest.java
@@ -24,4 +24,53 @@ class GameTest {
     void player1_wins_with_rock_vs_cpu_scissors() {
         // To be continued
     }
+
+//    @Test
+//    void name() {
+//        RandomMove randomMove = mock(RandomMove.class);
+//        when(randomMove.randomMove()).thenReturn("scissors");
+//        Game game = new Game(randomMove, "arne");
+//
+//        game.throwGesture("rock");
+//
+//        assertEquals(1, game.getPlayerScore());
+//        assertEquals(0, game.getComputerScore());
+//    }
+//
+//    @Test
+//    void name1() {
+//        GameControllerStatus gameControllerStatus = GameControllerStatus.MAKE_MOVE;
+//        GameController gameController = new GameController(gameControllerStatus);
+//
+//        gameController.playerInput("rock");
+//
+//        assertEquals(GameControllerStatus.MAKE_MOVE, gameController.getStatus());
+//        assertEquals(1, game.getPlayerScore());
+//        assertEquals(0, game.getComputerScore());
+//    }
+//
+//    @Test
+//    void name2() {
+//        GameControllerStatus gameControllerStatus = GameControllerStatus.ENTER_NAME;
+//        GameController gameController = new GameController(gameControllerStatus);
+//
+//        gameController.playerInput("arne");
+//
+//        assertEquals(GameControllerStatus.MAKE_MOVE, gameController.getStatus());
+//    }
+//
+//    @Test
+//    void name3() {
+//        GameControllerStatus gameControllerStatus = GameControllerStatus.MAKE_MOVE;
+//        GameController gameController = new GameController(gameControllerStatus);
+//
+//        gameController.playerInput("rock");
+//        gameController.playerInput("rock");
+//        gameController.playerInput("rock");
+//
+//        assertEquals(GameControllerStatus.GAME_ENDED, gameController.getStatus());
+//        assertEquals(3, game.getPlayerScore());
+//        assertEquals(0, game.getComputerScore());
+//    }
+
 }
